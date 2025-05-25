5ec5d13249419a43eb67acc35c5753e1bfd7a941
SMKM <svargas@desma.com.mx>
2025-05-26 05:50:18 +0600

    fixed conflix with treesitter repeated config

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -14,11 +14,7 @@ return {
           enable = true,
           -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
           disable = function(lang, buf)
-<<<<<<< HEAD
-            local max_filesize = 100 * 1024   -- 100 KB
-=======
             local max_filesize = 100 * 1024 -- 100 KB
->>>>>>> 03cdbc1c28c7af048c9fbc827279290ea2e99ebd
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
               return true
